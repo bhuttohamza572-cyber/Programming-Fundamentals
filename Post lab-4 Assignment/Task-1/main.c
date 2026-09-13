d13c8845b84cd53433b0cb5f98f9d8354b3fbf07
bhuttohamza572-cyber <bhuttohamza572@gmail.com>
2026-09-13 15:22:35 +0500

    Task 1

---
+++ Post lab-4 Assignment/Task-1/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+   int marks,income;
+    printf("Enter the marks: ");
+    scanf("%d",&marks);
+
+    printf("Enter the family income: ");
+    scanf("%d",&income);
+
+    if(marks>=80 || income<50000){
+    printf("Scholarship Granted");
+    }
+    else printf("Not eligible for scholarship");
+    
+}
