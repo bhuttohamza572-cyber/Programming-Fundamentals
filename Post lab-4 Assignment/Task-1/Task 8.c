7ea50e72641bae63feaaa2ccbe9d46805677ee88
bhuttohamza572-cyber <bhuttohamza572@gmail.com>
2026-09-13 16:28:19 +0500

    Create Task 8.c

---
+++ Post lab-4 Assignment/Task-1/Task 8.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+   int marks;
+    printf("Enter the marks: ");
+    scanf("%d",&marks);
+
+    if(marks>=50){
+        printf("Pass");
+    }
+    else printf("Fail");
+}
